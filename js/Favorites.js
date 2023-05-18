22d402d75434f5de6f430bd2ee5d2c43a78c33e2
Gabriel Rocha Andrade Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2023-05-18 10:29:59 -0300

    Criando a lógica de modificar o html
    Continuando o desenvolvimento do código JavaScript, criando a lógica de modificar a tabelha HTML através dos métodos da classe FavoritesView e simulando o carregamento de dados na classe Favorites

--- js/Favorites.js
+++ js/Favorites.js
@@ -1,6 +1,30 @@
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root)
+    this.load()
+  }
+
+  load() {
+    this.entries = [
+      {
+        login: 'GabrielRARodrigues',
+        name: 'Gabriel Rodrigues',
+        public_repos: '63',
+        followers: '1'
+      },
+      {
+        login: 'diego3g',
+        name: 'Diego Fernandes',
+        public_repos: '76',
+        followers: '120000'
+      }
+    ]
+  }
+
+  delete(user) {
+    const filteredEntries = this.entries.filter(
+      entry => entry.login !== user.login
+    )
   }
 }
 
@@ -8,17 +32,60 @@ export class FavoritesView extends Favorites {
   constructor(root) {
     super(root)
 
+    this.tbody = this.root.querySelector('table tbody')
+
     this.update()
   }
 
   update() {
     this.revomeAllTr()
+
+    this.entries.forEach(user => {
+      const row = this.createRow()
+      row.querySelector(
+        '.user img'
+      ).src = `https://github.com/${user.login}.png`
+      row.querySelector('.user img').alt = `Imagem de ${user.name}`
+      row.querySelector('.user p').textContent = user.name
+      row.querySelector('.user span').textContent = user.login
+      row.querySelector('.repositories').textContent = user.public_repos
+      row.querySelector('.followers').textContent = user.followers
+
+      row.querySelector('.remove').onclick = () => {
+        const isOK = confirm('Tem certeza que deseja deletar essa linha ?')
+
+        if (isOK) {
+          this.delete(user)
+        }
+      }
+
+      this.tbody.append(row)
+    })
+  }
+
+  createRow() {
+    const tr = document.createElement('tr')
+
+    tr.innerHTML = `          
+      <td class="user">
+        <img
+          src="https://github.com/GabrielRARodrigues.png"
+          alt="Imagem de Gabriel Rodrigues"
+        />
+        <a href="https://github.com/GabrielRARodrigues">
+          <p>Gabriel Rodrigues</p>
+          <span>GabrielRARodrigues</span>
+        </a>
+      </td>
+      <td class="repositories">62</td>
+      <td class="followers">1</td>
+      <td><button class="remove">&times;</button></td>`
+
+    return tr
   }
 
   revomeAllTr() {
-    const tbody = this.root.querySelector('table tbody')
-
-    tbody.querySelectorAll('tr').forEach(tr => {
+    this.tbody.querySelectorAll('tr').forEach(tr => {
       tr.remove()
     })
   }
